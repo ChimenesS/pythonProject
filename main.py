28827b0a87d3220ff4605c092367ed30ee011396
ChimenesS <chimenesm@hotmail.com>
2023-04-22 01:29:40 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#caular a area de um triangulo nao equilatero usando as informacoes do usuario
+h = int(input('digite o valor da altura do triangulo'))
+b = int(input('digiteo valor da base do traingulo'))
+a = print({h*b/2})
